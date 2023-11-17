4aa8bdba1834f98c0816b8b429c2570e478bf80e
gabumwaingo <gabumwaingo@gmail.com>
2023-11-17 16:43:57 +0000

    Task 1

--- 0x03-user_authentication_service/user.py
+++ 0x03-user_authentication_service/user.py
@@ -9,6 +9,7 @@ Base = declarative_base()
 
 
 class User(Base):
+    """ User class that will be mapped to user table """
     __tablename__ = 'users'
     id = Column(Integer, primary_key=True)
     email = Column(String(250), nullable=False)
