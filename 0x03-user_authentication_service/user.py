8a0f81e2a594805903ceda11604b2b620bcb0693
gabumwaingo <gabumwaingo@gmail.com>
2023-11-17 02:19:39 +0000

    Task 0

---
+++ 0x03-user_authentication_service/user.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+""" sqlalchemy class that map to user table """
+
+
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+
+class User(Base):
+    __tablename__ = 'users'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(250), nullable=False)
+    hashed_password = Column(String(250), nullable=False)
+    session_id = Column(String(250), nullable=True)
+    reset_token = Column(String(250), nullable=True)
