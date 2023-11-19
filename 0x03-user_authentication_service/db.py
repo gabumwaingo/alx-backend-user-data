eb2fa2bd95aa7a86e034c86a61694ae1cce9e721
gabumwaingo <gabumwaingo@gmail.com>
2023-11-19 06:41:29 +0000

    Task 2

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -5,6 +5,8 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm.session import Session
+from sqlalchemy.orm.exc import NoResultFound
+from sqlalchemy.exc import InvalidRequestError
 
 
 from user import Base, User
@@ -37,3 +39,14 @@ class DB:
         self._session.add(new_user)
         self._session.commit()
         return new_user
+
+    def find_user_by(self, **kwargs):
+        """Find a user based on provided filters
+        """
+        try:
+            user = self._session.query(User).filter_by(**kwargs).first()
+            if user is None:
+                raise NoResultFound("No user found with provided filters")
+            return user
+        except NoResultFound as e:
+            raise e
