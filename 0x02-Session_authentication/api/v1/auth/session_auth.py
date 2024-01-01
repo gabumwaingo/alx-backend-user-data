54d6bfbd4f7e7fe49c4b3d72b2dfc7b0b13938d3
gabumwaingo <gabumwaingo@gmail.com>
2024-01-01 20:36:01 +0000

    Task 8

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -1,90 +1,112 @@
 #!/usr/bin/env python3
-""" Module of Session Auth
+"""Session authentication module for the API.
 """
-from api.v1.auth.auth import Auth
+
+
+from uuid import uuid4
+
 from models.user import User
-from typing import Dict, TypeVar
-from uuid import uuid4, UUID
+
+from .auth import Auth
 
 
 class SessionAuth(Auth):
-    """ Auth Class """
-    user_id_by_session_id: Dict = {}
+    """Session authentication class that inherits from Auth class.
+
+    Args:
+        Auth (type): Class inherited from.
+    """
+    user_id_by_session_id = {}
 
     def create_session(self, user_id: str = None) -> str:
+        """Creates a Session ID for a user_id.
+
+        Args:
+            user_id (str, optional): The ID of user to create a session for.
+            Defaults to None.
+
+        Returns:
+            str: The session ID if the user ID is valid, None otherwise.
         """
-            Make a new Session and register in the class
-
-            Args:
-                user_id: Identificator of the user_id
-
-            Return:
-                Session ID
-        """
-        if user_id is None or type(user_id) is not str:
-            return None
-
-        session_id: str = str(uuid4())
-        self.user_id_by_session_id[session_id] = user_id
-
-        return session_id
+        # If user_id is not None and is of type str
+        if type(user_id) is str:
+            # Generate a session ID using the uuid module's uuid4() function
+            session_id = str(uuid4())
+            # Store the mapping of session ID to user ID in the dictionary,
+            # Use this Session ID as key of dictionary user_id_by_session_id,
+            # the value for this key must be user_id
+            self.user_id_by_session_id[session_id] = user_id
+            # Return the session ID
+            return session_id
 
     def user_id_for_session_id(self, session_id: str = None) -> str:
+        """Retrieves the user ID for a given session ID.
+
+        Args:
+            session_id (str, optional): The session ID to retrieve the user
+            ID for.
+            Defaults to None.
+
+        Return None if session_id is None.
+        Return None if session_id is not a string.
+        Return the value (the User ID) for the key session_id in the dictionary
+        user_id_by_session_id.
+
+        Returns:
+            str: The user ID if the session ID is valid, None otherwise.
         """
-            Make a user ID based in session id
+        # If session_id is not None or is a string
+        if type(session_id) is str:
+            # Return the value (user ID) for the key session_id in dictionary
+            # user_id_by_session_id
+            return self.user_id_by_session_id.get(session_id)
 
-            Args:
-                session_id: String of the session
+    def current_user(self, request=None) -> User:
+        """Returns a User instance based on a cookie value.
 
-            Return:
-                User ID
+        Args:
+            request (flask.request, optional): The request object containing
+            the session cookie.. Defaults to None.
+
+        Use self.session_cookie(...) & self.user_id_for_session_id(...)
+        to return the User ID based on the cookie _my_session_id.
+
+        Returns:
+            User: A User instance, if a User can be found based on the value
+            of the cookie. Otherwise, returns None.
         """
-        if session_id is None or type(session_id) is not str:
-            return None
-
-        user_id: str = self.user_id_by_session_id.get(session_id)
-
-        return user_id
-
-    def current_user(self, request=None):
-        """
-            Take the session cookie and the user id
-            and show the user
-
-            Args:
-                request: Look the request
-
-            Return:
-                User instance based in cooikie
-        """
-        session_id: str = self.session_cookie(request)
-        user_id: str = self.user_id_for_session_id(session_id)
-        user: TypeVar('User') = User.get(user_id)
-
+        # Retrieve the value of the _my_session_id cookie from the request
+        session_id = self.session_cookie(request)
+        # Look up the corresponding User ID based on the session_id
+        user_id = self.user_id_for_session_id(session_id)
+        # Retrieve the User instance from the database based on the user_id
+        user = User.get(user_id)
+        # Return the User instance
         return user
 
     def destroy_session(self, request=None):
-        """ Destroy the auth session if this
+        """Delete the user session (log out the user) based on the session
+        ID cookie in the request.
 
-        Return:
-            Destuction
+        Args:
+            request (flask.request, optional): The Flask request object.
+            Defaults to None.
+
+        Returns:
+            bool: True if the session was destroyed, False otherwise.
         """
-        if request is None:
+        session_id = self.session_cookie(request)
+        user_id = self.user_id_for_session_id(session_id)
+        # If the request is equal to None, return False
+        # If the request doesn’t contain the Session ID cookie, return False
+        # If the Session ID of the request is not linked to any User ID,
+        # return False
+        if (request is None or session_id is None) or user_id is None:
             return False
-
-        session_id: str = self.session_cookie(request)
-
-        if session_id is None:
-            return False
-
-        user_id: str = self.user_id_for_session_id(session_id)
-
-        if user_id is None:
-            return False
-
-        try:
+        # Otherwise, delete in self.user_id_by_session_id the Session ID (as
+        # key of this dictionary) and return True
+        if session_id in self.user_id_by_session_id:
             del self.user_id_by_session_id[session_id]
-        except Exception:
-            pass
-
+        # Return True if the session was destroyed successfully
         return True
+
